386c9a4d52bda7ae4d18f895bb55464871b62a92
jorgetovar <jorgetovar621@gmail.com>
2022-02-25 15:54:03 -0500

    Initial commit

---
+++ infra/backend-state/main.tf
@@ -0,0 +1,45 @@
+# CREATE AN S3 BUCKET AND DYNAMODB TABLE TO USE AS A TERRAFORM BACKEND
+terraform {
+  required_version = ">= 1.1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 4.0"
+    }
+  }
+}
+
+
+# CONFIGURE OUR AWS CONNECTION
+provider "aws" {
+  region = "us-east-1"
+}
+
+# CREATE THE S3 BUCKET
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "vite-aws-terraform"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# CREATE THE DYNAMODB TABLE
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "vite-aws-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
