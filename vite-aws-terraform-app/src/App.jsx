9fe68b5cd69edf5987baca97289f8a2cd0600d45
Jorge Tovar <jtovar@atlassian.com>
2022-12-20 09:35:34 -0500

    Add web page link

--- vite-aws-terraform-app/src/App.jsx
+++ vite-aws-terraform-app/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + AWS + Terraform + CircleCI!</p>
-        <p>This is a demo by @jorgetovar</p>
+        <p>Hello World! AWS + Terraform + CircleCI!</p>
+        <p>This is a demo by https://www.jorgetovar.dev/</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
